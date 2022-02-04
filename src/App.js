906f368d28a7922f649267823cac2c7481217f20
enri364 <enri364@gmail.com>
2022-02-04 17:14:05 +0100

    Atualización componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+import Footer from './Footer/Footer.js';
+import Header from './Header/Header.js';
+import HomeLogin from './HomeLogin/HomeLogin.js';
+import HomeSinLogin from './HomeSinLogin/HomeSinLogin.js';
+
+import Login from './Login/Login.js';
+import MiPerfil from './MiPerfil/MiPerfil.js';
+import PerfilUsuario from './PerfilUsuario/PerfilUsuario.js';
+import Registro from './Registro/Registro.js';
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header></Header>
+      <div style={{ backgroundColor: "yellow", width: "100%", height: "150px" }}>
+      <Routes>
+        <Route path="/" element={<HomeSinLogin />} />
+        <Route path="/homelogin" element={<HomeLogin />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/miperfil" element={<MiPerfil />} />
+        <Route path="/perfilusuario" element={<PerfilUsuario />} />
+        <Route path="/registro" element={<Registro />} />
+      </Routes>
+      </div>
+      
+      <Footer></Footer>
+    </Router>
   );
 }
-
-export default App;
